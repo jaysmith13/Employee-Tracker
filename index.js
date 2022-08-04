055d50d92f2c4d4f9f10da4cad358adc96464065
jaysmith13 <janeasmith13@gmail.com>
2022-08-03 19:35:22 -0600

    Update index.js
    adding connections / finish initial questions and prompts

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+const { createConnection } = require('net');
 require('console.table');
 
 // Intial questions and prompts
 
 const promptMessages = {
     viewAllEmployees: "View All Employees",
-    ViewByDepartment:
-    ViewByManager:,
-    addEmployee:,
-    removeEmployee:, 
+    ViewByDepartment:"View All Employess by Department",
+    ViewByManager:"View all Employees By Manager",
+    addEmployee: "Add an Employee",
+    removeEmployee:"Remove Employee", 
+    updateRole:"Update Employee Role",
+    updateEmployeeManager:"update Employee Manager",
     exit: "Exit"
 };
+// Connections
+createConnection.connect(err => {
+    if (err) throw err;
+    prompt();
+})
