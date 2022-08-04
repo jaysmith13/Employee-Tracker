fe872ef07f231505f3cf2780ac3bdd7f9f464eea
jaysmith13 <janeasmith13@gmail.com>
2022-08-04 01:03:45 -0600

    Update seed.sql
    added employee select and join

--- Assets/SQL/seed.sql
+++ Assets/SQL/seed.sql
@@ -3,3 +3,12 @@ SELECT role.id, role.title FROM role ORDER BY role.id;
 SELECT * FROM employee;
 
 SELECT first_name, last_name, role_id FROM employee WHERE employee.id = 4;
+
+SELECT department.id, department.name FROM department ORDER BY department.id;
+
+SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, '', manager.last_name) AS manager
+FROM employee
+LEFT JOIN employee manager on manager.id = employee.manager_id
+INNER JOIN role on (role.id = employee.role_id)
+INNER JOIN department on (department.id = role.department_id)
+ORDER BY employee.id;
