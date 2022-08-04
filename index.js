db6e83580bd7320f645f71650d74a49586adedad
jaysmith13 <janeasmith13@gmail.com>
2022-08-03 19:38:49 -0600

    update connection section

--- index.js
+++ index.js
@@ -19,4 +19,13 @@ const promptMessages = {
 createConnection.connect(err => {
     if (err) throw err;
     prompt();
-})
+});
+
+const connection = mysql.createConnection({
+    host:'localhost',
+
+    user:'root',
+    port:'3306',
+    password:'',
+    database:'employees'
+});
