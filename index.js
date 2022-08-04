b39104553065a96ab6cedf5f015ac5715e8d3d2c
jaysmith13 <janeasmith13@gmail.com>
2022-08-03 19:30:50 -0600

    adding initial questions

---
+++ index.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+require('console.table');
+
+// Intial questions and prompts
+
+const promptMessages = {
+    viewAllEmployees: "View All Employees",
+    ViewByDepartment:
+    ViewByManager:,
+    addEmployee:,
+    removeEmployee:, 
+    exit: "Exit"
+};
