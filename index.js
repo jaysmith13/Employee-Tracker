2760fa1bcd1c3698d17da5a6bff3375efabc36a1
jaysmith13 <janeasmith13@gmail.com>
2022-08-09 01:38:41 -0600

    update
    updates index and files

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 const { createConnection } = require('net');
+const { connect } = require('http2');
+const { from } = require('rxjs');
+const { join } = require('path');
 require('console.table');
 
 // Intial questions and prompts
@@ -21,21 +24,12 @@ createConnection.connect(err => {
     prompt();
 });
 
-const connection = mysql.createConnection({
-    host:'localhost',
-
-    user:'root',
-    port:'3306',
-    password:'',
-    database:'employees'
-});
-
 function prompt(){
     inquirer
     .prompt({
         name:'action',
         type:'list',
-        message:'Which option would you like to do?',
+        message:'Which action would you like to do?',
         choices:[
             promptMessages.viewAllEmployees,
             promptMessages.ViewByDepartment,
@@ -53,16 +47,85 @@ function prompt(){
     switch (answer.action){
         case promptMessages.viewAllEmployees:
             viewAllEmployees();
+                break;
+        case promptMessages.ViewByDepartment:
+             ViewByDepartment();
+                break;
+        case promptMessages.addEmployee:
+            addEmployee();
+                break;
+        case promptMessages.ViewByManager:
+            ViewByManager();
+                break;
+        case promptMessages.removeEmployee:
+            remove('delete');
+        case promptMessages.viewRoles:
+            viewRoles();
             break;
+        case promptMessages.updateRole:
+            updateRole()
+            break;
+        case promptMessages.exit:
+            createConnection.end();
+            break;
+
     }
 })
 
 }
+function viewAllDepartment() {
+    const query = `select department.name, role.title, employee.id, employee.first_name, employee.last_name
+    from employee 
+    left join role on (role.id = employee.role_id)
+    left join department on (department.id = role.department_id)
+    Order by department.name;`;
+    db.query(query,(err,res) => {
+        if(err) throw err;
+    }
+    ,console.table(res));
+    prompt();
+};
+function viewAllEmployees() {
+    const query = `select employee.id, role.title, employee.first_name, employee.last_name, department.name, 
+    from employee 
+    left join employee manager on (manager.id = employee.manager_id)
+    Inner join department on (department.id = role.department_id)
+    Inner join role on (role.id=employee.role_id)
+    Order by employee.name;`;
+    db.query(query,(err,res) => {
+        if(err) throw err;
+    }
+    ,console.table(res));
+    prompt();
+};
+
+
 
 // functions selections
-
-
-
+function askName(){
+    return ([
+        {
+            name: "first",
+            type: "input",
+            message: "Input first name"
+        },
+        {
+            name: "last",
+            type: "input",
+            message: "Input last name"
+        }
+    ])
+};
+function askId(){
+    return ([{
+        name: "name",
+        type: "input",
+        message: "Input the employee ID"
+    }]);
+};
+function remove(input){
+    const prompt;
+}
 
 
 
